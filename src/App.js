e158903f8447662791088fb8d61634f0e5c14e43
Jayasree <jspa9999@gmail.com>
2024-08-25 19:34:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Intermediate from './Component/Intermediate';
+
+function App() {
+  return (
+    <div>
+     <Intermediate/>
+    </div>
+  );
+}
+
+export default App;
